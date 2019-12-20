a1202811cf24e832e961bcdbf63e61fc782aa356
Talha-Jawed <jawedtalha0@gmail.com>
2019-12-20 21:44:48 +0500

    chacking

--- model/index.js
+++ model/index.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 var userSchema = new mongoose.Schema({
     name: String,
     email: String,
     age: Number
-})
+});
 
 const User = mongoose.model('user', userSchema);
 

a114ece1b507fdc6f2518ab28fbe6a2d3b377cf7
Talha-Jawed <jawedtalha0@gmail.com>
2020-01-03 20:37:07 +0500

    bug fix

--- model/index.js
+++ model/index.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 var userSchema = new mongoose.Schema({
     name: String,
     email: String,
-    age: Number,
-    date: String
+    age: String,
+    date: Number
 });
 
 const User = mongoose.model('user', userSchema);

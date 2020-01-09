ed22c9f36201873280a53cae5cc67e69072045ed
Talha-Jawed <jawedtalha0@gmail.com>
2020-01-09 23:58:23 +0500

    update

---
+++ routes/Update.js
@@ -0,0 +1,24 @@
+exports = module.exports = function (app) {
+    const express = require("express");
+    const router = express.Router();
+    const User = require('../model');
+
+    router.put("/add", (req , res)=>{
+        console.log(res,'-=-=-=');
+        
+        User.update(
+            { _id: "5decbb064ee4f8269c9f14d1" },
+            {
+                $set:
+                    { name: "ali" , "age": "12456" }
+            },
+            (err, result) => {
+                console.log(err, '*/*/*/*', result);
+
+                res.send(result);
+            });
+    })
+
+    app.use('/Update', router)
+
+}

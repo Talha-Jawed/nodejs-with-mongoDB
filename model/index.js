14a2bb96b352547a54d296d28f6d773754140b11
Talha-Jawed <jawedtalha0@gmail.com>
2020-01-12 23:32:51 +0500

    required

--- model/index.js
+++ model/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 var userSchema = new mongoose.Schema({
-    name: String,
+    name: { type: String, required: true },
     email: String,
     age: String,
     date: Number

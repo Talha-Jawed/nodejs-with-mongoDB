cadc3f5f641e7d6c85dc5922f15d50fa212d0c74
Talha-Jawed <jawedtalha0@gmail.com>
2020-01-18 23:15:48 +0500

    new schema

--- model/index.js
+++ model/index.js
@@ -4,9 +4,22 @@ var userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: String,
     age: String,
+    status: { type: Object },
+    date: Number
+});
+
+var userDataSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: String,
+    age: String,
+    status: { type: Object },
     date: Number
 });
 
 const User = mongoose.model('user', userSchema);
+const UserData = mongoose.model('userData', userDataSchema);
 
-module.exports = User;
+module.exports = {
+    User,
+    UserData
+}
